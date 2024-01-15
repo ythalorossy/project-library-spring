b3b9038114818a08b87a795504f81ba296572ad1
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-15 13:13:48 -0500

    backup the legace authorization server

--- auth-service-bkp/src/main/java/com/library/authservice/AuthServiceApplication.java
+++ auth-service-bkp/src/main/java/com/library/authservice/AuthServiceApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableResourceServer
 @EnableMongoRepositories(basePackages = {"com.library.authservice.repository"})
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
