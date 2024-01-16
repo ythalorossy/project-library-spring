c677ed1a35ca8eb029d46ed4d10f17c36d95bee8
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-16 17:17:05 -0500

    setup actuator

---
+++ books-service/src/main/java/com/library/booksservice/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.library.booksservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/actuator/**", "/actuator/info**").permitAll()
+                .anyRequest().permitAll()
+                )
+                ;
+        
+                return http.build();
+    }
+
+}
