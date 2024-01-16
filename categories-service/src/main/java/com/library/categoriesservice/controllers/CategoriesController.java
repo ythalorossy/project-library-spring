75eaa145ff51df999db547113208b1d9924d6482
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-15 19:49:05 -0500

    upgraded categories app

--- categories-service/src/main/java/com/library/categoriesservice/controllers/CategoriesController.java
+++ categories-service/src/main/java/com/library/categoriesservice/controllers/CategoriesController.java
@@ -5,14 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = {"/"})
 public class CategoriesController {
 
-    @GetMapping(path = {"is-running"})
+    @GetMapping("/is-running")
     public ResponseEntity<String> isRunning() {
 
         String isRunning = "Categories Service is running. Current Time: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
