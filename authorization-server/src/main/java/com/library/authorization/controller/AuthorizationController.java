a092950988124d8fa335479300ceba9a814c0eaa
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-18 04:55:40 -0500

    tidy up the authorization controller

--- authorization-server/src/main/java/com/library/authorization/controller/AuthorizationController.java
+++ authorization-server/src/main/java/com/library/authorization/controller/AuthorizationController.java
@@ -2,57 +2,56 @@ package com.library.authorization.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.library.authorization.service.RegisteredClientService;
+
 import java.time.Instant;
 import java.util.Collections;
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-record RequestClientRegistrationDto(String clientId, String redirectUri) {
+record RequestClientRegistration(String clientId, String password, String redirectUri) {
+};
+
+record ResponseClientRegistration(String clientId, String password) {
 };
 
 @RestController
 @RequestMapping("/api")
 public class AuthorizationController {
 
-    private RegisteredClientRepository registeredClientRepository;
+    private RegisteredClientService registeredClientService;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthorizationController(RegisteredClientRepository registeredClientRepository) {
-        this.registeredClientRepository = registeredClientRepository;
+    public AuthorizationController(RegisteredClientService registeredClientService, PasswordEncoder passwordEncoder ) {
+        this.registeredClientService = registeredClientService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
-    public ResponseEntity<RegisteredClient> registerClient(
-            @RequestBody RequestClientRegistrationDto requestClientRegistrationDto) {
+    public ResponseEntity<ResponseClientRegistration> registerClient(
+            @RequestBody RequestClientRegistration requestClientRegistrationDto) {
 
-        final RegisteredClient client = Optional
-                .ofNullable(registeredClientRepository.findByClientId(requestClientRegistrationDto.clientId()))
-                .orElseGet(() -> {
-                    RegisteredClient registeredClient = getRegisteredClient(requestClientRegistrationDto);
+            RegisteredClient registeredClient = getRegisteredClient(requestClientRegistrationDto);
+            
+            this.registeredClientService.registerClient(registeredClient);
 
-                    registeredClientRepository.save(registeredClient);
-
-                    return registeredClient;
-                });
-
-        return ResponseEntity.ok(client);
+        return ResponseEntity.ok(new ResponseClientRegistration(registeredClient.getClientId(), registeredClient.getClientSecret()));
     }
 
-    private RegisteredClient getRegisteredClient(RequestClientRegistrationDto requestClientRegistrationDto) {
+    private RegisteredClient getRegisteredClient(RequestClientRegistration requestClientRegistrationDto) {
         RegisteredClient registeredClient = RegisteredClient
                 .withId(UUID.randomUUID().toString())
                 .clientId(requestClientRegistrationDto.clientId())
-                .clientSecret(new BCryptPasswordEncoder().encode(requestClientRegistrationDto.clientId()))
+                .clientSecret(this.passwordEncoder.encode(requestClientRegistrationDto.clientId()))
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
