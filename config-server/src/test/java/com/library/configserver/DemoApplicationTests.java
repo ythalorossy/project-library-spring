5b8c30dc4f23ee90f622613deee00935d7448ca8
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-23 14:06:09 -0500

    tidy up unnecessary imports

--- config-server/src/test/java/com/library/configserver/DemoApplicationTests.java
+++ config-server/src/test/java/com/library/configserver/DemoApplicationTests.java
@@ -1,7 +1,7 @@
 package com.library.configserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 
 // @SpringBootTest
