078998155d7cd9425c2c2d54d33d90a8cb2022c2
Ythalo Saldanha <ythalorossy@gmail.com>
2024-01-30 22:18:37 -0500

    configure service to run in random port and have eureka unique client name

--- books-service/src/main/java/com/library/booksservice/controllers/BooksController.java
+++ books-service/src/main/java/com/library/booksservice/controllers/BooksController.java
@@ -3,6 +3,7 @@ package com.library.booksservice.controllers;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BooksController {
 
+    record IsRunningResponse(String message, String currentTime, String port) {
+    }
+
+    private Environment environment;
+
+    public BooksController(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping("/is-running")
-    public ResponseEntity<String> isRunning() {
+    public ResponseEntity<IsRunningResponse> isRunning() {
 
-        String isRunning = "Books Service is running. Current Time: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String localServerPort = environment.getProperty("local.server.port");
 
-        return ResponseEntity.ok(isRunning);    
+        IsRunningResponse response = new IsRunningResponse(
+                "Books Service is running",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                localServerPort);
+
+        return ResponseEntity.ok(response);
     }
 }
--- categories-service/src/main/java/com/library/categoriesservice/controllers/CategoriesController.java
+++ categories-service/src/main/java/com/library/categoriesservice/controllers/CategoriesController.java
@@ -3,6 +3,7 @@ package com.library.categoriesservice.controllers;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CategoriesController {
 
-    @GetMapping("/is-running")
-    public ResponseEntity<String> isRunning() {
-
-        String isRunning = "Categories Service is running. Current Time: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-        return ResponseEntity.ok(isRunning);
+    record IsRunningResponse(String message, String currentTime, String port) {
     }
-    
+
+    private Environment environment;
+
+    public CategoriesController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/is-running")
+    public ResponseEntity<IsRunningResponse> isRunning() {
+
+        String localServerPort = environment.getProperty("local.server.port");
+
+        IsRunningResponse response = new IsRunningResponse(
+                "Books Service is running",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                localServerPort);
+
+        return ResponseEntity.ok(response);
+    }
+
 }
